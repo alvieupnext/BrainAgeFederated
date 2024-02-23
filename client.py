8a3bed10b861faafe686b3b4ad86f9e8a35e51f8
alvieupnext <alvarovargasguerrero@hotmail.com>
2024-02-23 22:08:28 +0100

    Implemented flower clients

---
+++ client.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from random import random
+
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+
+from centralized import (get_test_loader, get_train_valid_loader, group_datasets,
+                         train, validate, convert_state_dict, load_model, DEVICE)
+from collections import OrderedDict
+import flwr as fl
+import torch.nn as nn
+import pandas as pd
+import torch
+
+def set_parameters(model, parameters):
+    params_dict = zip(model.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+    model.load_state_dict(state_dict, strict=True)
+
+# net = load_model()
+#
+# trainloader, valloader = get_train_valid_loader('patients_dataset_6326_train.csv', batch_size=4, random_seed=10, aug='none', kcrossval=None, icross=-1)
+
+
+project_name = 'test'
+save_dir = './utils/models/' + project_name + "/"
+
+class FlowerClient(fl.client.NumPyClient):
+
+    def __init__(self, net, trainloader, valloader, testloader, cid, name=None):
+      self.net = net
+      self.trainloader = trainloader
+      self.valloader = valloader
+      self.cid = cid
+      self.name = name
+      self.testloader = testloader
+
+    def get_parameters(self, config):
+        return [val.cpu().numpy() for _, val in self.net.state_dict().items()]
+
+    def set_parameters(self, parameters):
+        set_parameters(self.net, parameters)
+
+    def fit(self, parameters, config):
+        set_parameters(self.net, parameters)
+        friendly_name = self.name or self.cid
+        model_save_path = save_dir + friendly_name + "/" + datetime.now().strftime('{}_%d-%m-%y-%H_%M.pt'.format(project_name))
+        train(self.net, self.trainloader, self.valloader, 1, model_save_path)
+        return self.get_parameters({}), len(self.trainloader), {}
+
+    def evaluate(self, parameters, config):
+        set_parameters(self.net, parameters)
+        val_losses, _, _, _, _, val_mae = validate(self.net, self.testloader)
+        return float(val_losses), len(self.testloader), {}
