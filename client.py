7c9cad1d51524b1eeb7d1b82957b7300a43b2176
alvieupnext <alvarovargasguerrero@hotmail.com>
2024-02-28 21:39:54 +0100

    FedProxClient

--- client.py
+++ client.py
@@ -44,12 +44,14 @@ class FlowerClient(fl.client.NumPyClient):
     def set_parameters(self, parameters):
         set_parameters(self.net, parameters)
 
-    def train(self, config, model_save_path, losses_save_path, criterion, optimizer, scheduler, patience=5):
+    def train(self, config, model_save_path, losses_save_path, criterion, optimizer, scheduler, parameters, patience=5):
         best_loss = 1e9
         num_bad_epochs = 0
         is_new_best = False
         server_round = config["server_round"]
         epochs = config["local_epochs"]
+        #Set the parameters of the model to the parameters received
+        set_parameters(self.net, parameters)
 
         for epoch in range(epochs):
           if num_bad_epochs >= patience:
@@ -90,7 +92,6 @@ class FlowerClient(fl.client.NumPyClient):
         server_round = config["server_round"]
 
         print(f"[Client {self.cid}, friendly name {self.name}, round {server_round}] fit, config: {config}")
-        set_parameters(self.net, parameters)
         friendly_name = str(self.name) or str(self.cid)
         client_save_dir = self.save_dir + friendly_name + "/"
         #If the repository does not exist, create it
@@ -108,7 +109,9 @@ class FlowerClient(fl.client.NumPyClient):
         criterion = nn.L1Loss()
         optimizer = torch.optim.Adam(self.net.parameters(), lr=1e-4)
         scheduler = ReduceLROnPlateau(optimizer, mode='min', factor=0.5, patience=5)
-        self.train(config, model_save_path, losses_save_path, criterion, optimizer, scheduler)
+      #Make a copy of the global parameters
+        global_params = [param.clone().detach() for param in self.net.parameters()]
+        self.train(config, model_save_path, losses_save_path, criterion, optimizer, scheduler, global_params)
         return self.get_parameters({}), len(self.trainloader), {}
 
     def evaluate(self, parameters, config):
@@ -116,3 +119,55 @@ class FlowerClient(fl.client.NumPyClient):
         set_parameters(self.net, parameters)
         val_losses, _, _, _, _, val_mae = validate(self.net, self.valloader)
         return float(val_losses), len(self.valloader), {}
+
+#Client class for FedProx
+class FedProxClient(FlowerClient):
+  #Copy the train method from the parent class
+  def train(self, config, model_save_path, losses_save_path, criterion, optimizer, scheduler, parameters, patience=5):
+    # Read values from config
+    server_round = config["server_round"]
+    epochs = config["local_epochs"]
+    proximal_mu = config["proximal_mu"]
+
+    best_loss = 1e9
+    num_bad_epochs = 0
+    is_new_best = False
+
+    ##In FedProx, we do not need to override the net parameters, as we are using the proximal term
+
+    for epoch in range(epochs):
+      if num_bad_epochs >= patience:
+        print(f"Model reached patience: {patience}")
+        break
+      train_loss = self.train_epoch_proximal(criterion, optimizer, proximal_mu, parameters)
+      val_loss, corr, true_ages, pred_ages, ids_sub, mae = validate(self.net, self.valloader)
+      update_loss_df(losses_save_path, server_round, epoch, train_loss, val_loss)
+      is_new_best, best_loss, num_bad_epochs = check_improvement(val_loss, best_loss, self.net, model_save_path, epoch,
+                                                                 num_bad_epochs)
+      scheduler.step(val_loss)
+      lr = optimizer.param_groups[0]['lr']
+      print(
+        f'Epoch: {epoch + 1} of {epochs}, lr: {lr:.2E}, train loss: {train_loss:.2f}, valid loss: {val_loss:.2f}, corr: {corr:.2f}, best loss {best_loss:.2f}, number of epochs without improvement: {num_bad_epochs}')
+
+    return is_new_best, model_save_path
+
+  def train_epoch_proximal(self, criterion, optimizer, proximal_mu, parameters):
+    self.net.train()
+    train_loss = 0.0
+    train_count = 0
+    for data in tqdm(self.trainloader, leave=False):
+      im, age, _ = data
+      im = im.to(device=DEVICE, dtype=torch.float)
+      age = age.to(device=DEVICE, dtype=torch.float).reshape(-1, 1)
+      optimizer.zero_grad()
+      proximal_term = 0.0
+      for local_weights, global_weights in zip(self.net.parameters(), parameters):
+        proximal_term += torch.square((local_weights - global_weights).norm(2))
+      pred_age = self.net(im)
+      loss = criterion(pred_age, age) + (proximal_mu / 2) * proximal_term
+      loss.backward()
+      optimizer.step()
+      train_count += im.shape[0]
+      train_loss += loss.sum().detach().item()
+    train_loss /= train_count
+    return train_loss
