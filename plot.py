973327008d910956157e63346186f2e9f0fd237a
alvieupnext <alvarovargasguerrero@hotmail.com>
2024-04-30 12:38:35 +0200

    Refactored + made new plot functions

--- plot.py
+++ plot.py
@@ -177,7 +177,7 @@ def get_results(strategies, model, seeds, data, alias=None, kcrossval=False):
   return project_losses, client_losses
 
 # Define the plotting function
-def plot_losses(projects_losses, split='Dataset', mode='DWood', alias=None):
+def plot_centralized_losses(projects_losses, split='Dataset', mode='DWood', alias=None, nodes=6):
     # Set the aesthetic style of the plots
     sns.set(style="whitegrid")
 
@@ -204,7 +204,7 @@ def plot_losses(projects_losses, split='Dataset', mode='DWood', alias=None):
       verbose_mode = mode
 
     # Customize the plot
-    title = f'Losses Over Server Rounds ({split}, {verbose_mode})'
+    title = f'Losses Over Server Rounds ({split}, {verbose_mode}, {nodes} nodes)'
     if alias:
       title += f' ({alias})'
     plt.title(title, fontsize=16)
@@ -213,7 +213,7 @@ def plot_losses(projects_losses, split='Dataset', mode='DWood', alias=None):
     plt.legend(title='Strategies')
     sns.despine()
     #Save the plot at the plot folders
-    file_name = f'losses_over_server_rounds_{split}_{mode}'
+    file_name = f'losses_over_server_rounds_{split}_{mode}_{nodes}_nodes'
     if alias:
       file_name += f'_{alias}'
     path = os.path.join(plot_folder, f'{file_name}.pdf')
@@ -224,18 +224,17 @@ def plot_losses(projects_losses, split='Dataset', mode='DWood', alias=None):
     plt.show()
 
 
-def plot_client_losses(client_losses, client_number=12, split='Dataset', mode='DWood', alias='', kcrossval=False, runs=10):
+def plot_client_losses(client_losses, client_number=12, split='Dataset', mode='DWood', alias='', kcrossval=False, runs=10, columns=2):
   # Set the visual theme of the plots with Seaborn
   sns.set(style="whitegrid")
 
   #Get the amouunt of clients and turn it into a rows and column
   #The amount of rows varies and the columns are always 4
-  rows = int(np.ceil(client_number / 2))
-  columns = 2
+  rows = int(np.ceil(client_number / columns))
 
 
   # Create a 4x3 grid of subplots with shared x and y axes for uniform scale
-  fig, axes = plt.subplots(rows, columns, figsize=(14,14), sharex=True, sharey=True)
+  fig, axes = plt.subplots(rows, columns, figsize=(24,8), sharex=True, sharey=True)
   axes = axes.flatten()  # Flatten the 2D array of axes for easy iteration
 
   # From client_losses, retrieve all clients
@@ -268,8 +267,6 @@ def plot_client_losses(client_losses, client_number=12, split='Dataset', mode='D
       else:
         ax.set_xlabel('Server/Epoch')
       ax.set_ylabel('Loss')
-      # Display the legend to differentiate between training and validation loss
-      ax.legend()
 
   # Give a friendly name to the mode RW -> Random Weights
   if mode == 'RW':
@@ -277,13 +274,14 @@ def plot_client_losses(client_losses, client_number=12, split='Dataset', mode='D
   else:
     verbose_mode = mode
   # Set the main title for the entire figure, positioned above all subplots
-  title = f'Losses by Client ({split}, {verbose_mode})'
+
+  title = f'Losses by Client ({split}, {verbose_mode}, {nodes} nodes)'
   if alias:
     title += f' ({alias})'
   plt.suptitle(title, fontsize=20)
   # Adjust the layout to make sure there's no overlap between subplots
   plt.tight_layout()
-  file_name = f'Losses_by_client_{split}_{mode}'
+  file_name = f'Losses_by_client_{split}_{mode}_{nodes}_nodes'
   if alias:
     file_name += f'_{alias}'
   path = os.path.join(plot_folder, f'{file_name}.pdf')
@@ -346,11 +344,11 @@ def merge_client_losses(name, client_tuples):
   #Return the client_losses dictionary
   return client_losses
 
-def plot_merged_losses(client_losses, split='Dataset', mode='DWood', nodes=6, alias='', kcrossval=True):
+def plot_merged_losses(client_losses, strategy='FedAvg', split='Dataset', mode='DWood', nodes=6, alias='', kcrossval=True):
   #Make a plot with the seaborn style
   sns.set(style="whitegrid")
   #Everything will be plotted in a single figure
-  plt.figure(figsize=(10, 6))
+  plt.figure(figsize=(16, 10))
   #For every group in the client_losses dictionary, plot the training losses
   for group_name, (training_losses, val_loss_or_std, x_ticks) in client_losses.items():
     #Plot the training losses
@@ -370,7 +368,7 @@ def plot_merged_losses(client_losses, split='Dataset', mode='DWood', nodes=6, al
   else:
     verbose_mode = mode
   #Set the title of the plot
-  title = f'Individual Node Losses ({split}, {verbose_mode}, {nodes} nodes)'
+  title = f'Individual Node Losses ({strategy}, {split}, {verbose_mode}, {nodes} nodes)'
   #No alias is needed
   #Set the title
   plt.title(title, fontsize=16)
@@ -383,7 +381,7 @@ def plot_merged_losses(client_losses, split='Dataset', mode='DWood', nodes=6, al
   #Display the legend
   plt.legend()
   #Save the plot in the plot folder
-  file_name = f'Individual_Node_Losses_{split}_{mode}_{nodes}_nodes'
+  file_name = f'Individual_Node_Losses_{strategy}_{split}_{mode}_{nodes}_nodes'
   #Save the plot
   path = os.path.join(plot_folder, f'{file_name}.pdf')
   plt.savefig(path, format='pdf', dpi=300, bbox_inches='tight')
@@ -426,6 +424,43 @@ def plot_age_distribution(data, save_path=None, node=None):
   if save_path:
     plt.savefig(save_path, format='pdf', dpi=300, bbox_inches='tight')
   plt.show()
+
+def plot_multiple_age_distributions(dfs, profile, save_path=None):
+    # Determine the number of subplots needed
+    num_dfs = len(dfs)
+    cols = 3
+    rows = (num_dfs + cols - 1) // cols  # Calculate rows needed
+
+    # Create a figure with multiple subplots sharing the y-axis
+    fig, axes = plt.subplots(rows, cols, figsize=(10 * cols, 6 * rows), sharey=True)
+    sns.set(style="whitegrid")
+
+    fig.suptitle(f'Age Distribution Across Nodes ({profile})', fontsize=16, fontweight='bold')
+
+
+    # Flatten axes array if multiple rows and columns
+    if rows * cols > 1:
+      axes = axes.flatten()
+    else:
+      axes = [axes]
+
+    for ax, (node, data) in zip(axes, dfs.items()):
+      data['Age'].plot(kind='hist', bins=20, title=f'Age Distribution (Node: {node})',
+                       color='skyblue', edgecolor='black', rwidth=0.8, ax=ax)
+      ax.set_xlabel('Age')
+      ax.set_ylabel('Frequency')
+      ax.grid(True)
+
+    # Turn off any unused subplots
+    for i in range(len(dfs), rows * cols):
+      fig.delaxes(axes[i])
+
+    plt.tight_layout()  # Adjust layout to not cut off labels
+
+    # Save the plot if a path is provided
+    if save_path:
+      plt.savefig(f'{save_path}.pdf', format='pdf', dpi=300, bbox_inches='tight')
+    plt.show()
 
 # Plot the dataset_name distribution, make it a function
 # Only give a top 20 of the dataset_name, all other datasets should be classified as 'Other'
@@ -471,17 +506,35 @@ if __name__ == "__main__":
   model = ['DWood']
   seeds = [2]
   # data = ['Dataset']
-  data = ['Distribution_Gaussian', 'Distribution_Original', 'Distribution_Transition']
+  data = ['Distribution_Gaussian', 'Distribution_Original']
   nodes = 6
+  if nodes == 6:
+    data.append('Distribution_Transition')
   alias = f'{nodes}_Node'
   project_losses, client_losses = get_results(strategies, model, seeds, data, alias=alias, kcrossval=True)
-  all_merged_client_losses = {}
-  for project_name, client_tuples in client_losses.items():
-    merged_client_losses = merge_client_losses(project_name, client_tuples)
-    #Add the items of the merged_client_losses to the all_merged_client_losses dictionary
-    all_merged_client_losses.update(merged_client_losses)
-  print(all_merged_client_losses.keys())
-  plot_merged_losses(all_merged_client_losses, split='Distribution', mode=model[0], nodes=nodes, alias=alias, kcrossval=True)
+  # all_merged_client_losses = {}
+  # for project_name, client_tuples in client_losses.items():
+  #   merged_client_losses = merge_client_losses(project_name, client_tuples)
+  #   #Add the items of the merged_client_losses to the all_merged_client_losses dictionary
+  #   all_merged_client_losses.update(merged_client_losses)
+  # #Split the dictionary by what the strategy is (first word in the key, until the first underscore)
+  # project_names = list(all_merged_client_losses.keys())
+  # #Split the project names by the first underscore
+  # #Take the first element of the split project names
+  # strategies = [project_name.split('_')[0] for project_name in project_names]
+  # #Create a dictionary that holds the strategy and the project names
+  # strategy_project_names = {}
+  # for strategy, project_name in zip(strategies, project_names):
+  #   if strategy not in strategy_project_names:
+  #     strategy_project_names[strategy] = []
+  #   strategy_project_names[strategy].append(project_name)
+  # #For every strategy and project names
+  # for strategy, project_names in strategy_project_names.items():
+  #   #Get the client losses for the project names
+  #   strategy_client_losses = {project_name: all_merged_client_losses[project_name] for project_name in project_names}
+  #   #Plot the merged losses
+  #   plot_merged_losses(strategy_client_losses, strategy=strategy, split='Distribution', mode=model[0], nodes=nodes, alias=alias, kcrossval=True)
+  # plot_merged_losses(all_merged_client_losses, split='Distribution', mode=model[0], nodes=nodes, alias=alias, kcrossval=True)
   # print(project_losses.keys())
   # print(client_losses.keys())
   # # Print the results
@@ -489,5 +542,7 @@ if __name__ == "__main__":
   # # print(client_losses)
   # print(project_losses.values())
   # print(client_losses.values())
-  # plot_losses(project_losses, split='Distribution', mode=model[0], alias=alias)
-  # plot_client_losses(client_losses, nodes, split='Distribution', mode=model[0], alias=alias, kcrossval=True)
+  # From the project losses, remove all items with a key that have Gaussian in it
+  # project_losses = {key: value for key, value in project_losses.items() if 'Gaussian' not in key}
+  plot_centralized_losses(project_losses, split='Distribution', mode=model[0], nodes=nodes)
+  # plot_client_losses(client_losses, nodes, split='Distribution', mode=model[0], kcrossval=True, columns=3)
